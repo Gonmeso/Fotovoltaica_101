5ae80fd9c7cd9dd2b83b77a9eb2b643de243ce78
Gonmeso <gmellizosoto@gmail.com>
2016-09-13 00:55:00 +0200

    Actualización
    Adición foto España
    Trabajo solo con navbarPage y navlistPanel
    Comentados los anteriores

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 
 
+
 ##Función para la creación de la interfaz
 shinyUI(
   
@@ -14,52 +15,82 @@ shinyUI(
        strong("Fotovoltaica 101", align = "center"),
     
       #Tabs creadas, falta la parte de cada una 
-      tabPanel('Introducción'),
-      tabPanel('Localización'),
+      tabPanel('Introducción',
+               readLines("DefFotov.txt", n='1'),
+               div(img( src = 'EjFoto.jpg', height = 216, width = 300), style="text-align: center;")
+               
+               ),
+      tabPanel('Localización',
+               
+               
+               
+                 navlistPanel(
+                   tabPanel('Mapa',
+                            h3("Seleccione su posición", align = "center"),
+                            div(img( src = 'Espana.jpg', height = 345, width = 490), style="text-align: center;")
+                            ),
+                   
+                   tabPanel('Coordenadas',
+                            h3("La latitud de su posición es:"),
+                            br(),
+                            h3("La longitud de su posición es:")
+                            )
+                   
+                 )
+                 
+                 
+               
+               
+      ),
       tabPanel('Introducción'),
       tabPanel('Introducción')
       
       
-    ),        
+    )        
             
     
     ##Creación de la barra lateral, se puede incluir el main panel
-    sidebarLayout(
+    #sidebarLayout(
       
       
-      sidebarPanel(
+      #sidebarPanel(
         
-        navlistPanel(
-          
-          tabPanel('¿Qué es la energía fotovoltaica?'),
-          tabPanel('Ejemplos de módulos fotovoltaicos')
-          
-          )
+        # navlistPanel(
+        #   
+        #   tabPanel('¿Qué es la energía fotovoltaica?',
+        #            
+        #            readLines("DefFotov.txt", n='1')
+        #            
+        #            ),
+        #   
+        #   tabPanel('', 'Ejemplos de módulos fotovoltaicos')
+        #   
+        #   )
         
-        ),
+        #),
       
       ##sidebarPanel('Ejemplos'),
       
-      mainPanel( 
-        
-        # textOutput("DefFoto"),
-        # 
-        # imageOutput("EjFoto")
-        # 
-         readLines("DefFotov.txt", n='1'),
-                 style = "color:solidblack",
-        
-                 br(),
-                 br(),
-                 br(),
-        
-                 img( src = 'EjFoto.jpg', height = 216, width = 300, align = "left" , style='border:1px' )
-
-                )
+       #mainPanel( 'Hola'
+      #   
+      #   # textOutput("DefFoto"),
+      #   # 
+      #   # imageOutput("EjFoto")
+      #   # 
+      #    readLines("DefFotov.txt", n='1'),
+      #            style = "color:solidblack",
+      #   
+      #            br(),
+      #            br(),
+      #            br(),
+      #   
+      #            img( src = 'EjFoto.jpg', height = 216, width = 300, align = "left" , style='border:1px' )
+      # 
+        #         )
       
    
       
-    )
+   # )
     
     
     
