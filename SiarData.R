b9ae6016df3a968826fa3f54d9d94fdaee494623
Gonmeso <gmellizosoto@gmail.com>
2016-10-17 20:01:07 +0200

    Addición de datos

--- SiarData.R
+++ SiarData.R
@@ -1,2 +1,9 @@
 setwd("D:/TFG/Fotovoltaica 101/Fotovoltaica_101")
+library(readr)
 DatosSiar <- read.csv("data/Siar_Data.csv",  encoding="UTF-8")
+
+
+TryFata <- read.csv("data/Almeria/AL01_La Mojonera_01_01_2015_31_12_2015.csv", 
+                    header = TRUE,
+                    fileEncoding = "UTF-16LE",
+                    sep = ";")
